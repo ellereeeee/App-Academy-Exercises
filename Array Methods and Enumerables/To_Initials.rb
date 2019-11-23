f60901ab76bbac0d4aeca5b04c5bd9344f2737a5
ellereeeee <raellery@gmail.com>
2019-11-22 23:54:24 -0500

    Add some array methods solutions

---
+++ Array Methods and Enumerables/To_Initials.rb
@@ -0,0 +1,15 @@
+# Write a method to_initials that takes in a person's 
+# name string and returns a string representing 
+# their initials.
+
+def to_initials(name)
+  initials = []
+  
+  name.split(" ").each { |name| initials << name[0] }
+  
+  return initials.join("")
+end
+
+puts to_initials("Kelvin Bridges")      # => "KB"
+puts to_initials("Michaela Yamamoto")   # => "MY"
+puts to_initials("Mary La Grange")      # => "MLG"
