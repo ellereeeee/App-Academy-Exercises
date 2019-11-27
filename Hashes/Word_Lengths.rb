a7cba066a3a7652c9a3c848732b4052132b81bec
Richard Ellery <raellery@gmail.com>
2019-11-26 23:40:22 -0500

    Add hashes solutions

---
+++ Hashes/Word_Lengths.rb
@@ -0,0 +1,17 @@
+# Write a method word_lengths that takes in a sentence string and 
+# returns a hash where every key is a word of the sentence, and its' 
+# corresponding value is the length of that word.
+
+def word_lengths(sentence)
+  word_lengths_hash = {}
+  words = sentence.split(" ")
+  
+  words.each do |ele|
+    word_lengths_hash[ele] = ele.length
+  end
+  
+  return word_lengths_hash
+end
+
+puts word_lengths("this is fun") #=> {"this"=>4, "is"=>2, "fun"=>3}
+puts word_lengths("When in doubt, leave it out") #=> {"When"=>4, "in"=>2, "doubt,"=>6, "leave"=>5, "it"=>2, "out"=>3}
