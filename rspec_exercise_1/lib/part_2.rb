e10e9d4b90679b98219c3ddd8e9b0061c22fa2a6
ellereeeee <raellery@gmail.com>
2019-12-22 20:56:41 -0500

    Finish rspec exercise 1

--- rspec_exercise_1/lib/part_2.rb
+++ rspec_exercise_1/lib/part_2.rb
@@ -1,32 +1,34 @@
-# def hipsterfy(word)
-#   reversed = word.reverse
-#   vowels = "aeiou"
+def hipsterfy(word)
+  reversed = word.reverse
+  vowels = "aeiou"
 
-#   reversed.each_char.with_index do |char, idx|
-#     if vowels.include?(char)
-#       reversed[idx] = ""
-#       break
-#     end
-#   end
+  reversed.each_char.with_index do |char, idx|
+    if vowels.include?(char)
+      reversed[idx] = ""
+      break
+    end
+  end
 
-#   hipsterfied = reversed.reverse
-#   hipsterfied
-# end
+  hipsterfied = reversed.reverse
+  hipsterfied
+end
 
-# def vowel_counts(str)
-#   counts = Hash.new(0)
-#   vowels = "aeiou"
-#   str.each_char { |char| counts[char.downcase] += 1 if vowels.include?(char.downcase) }
-#   counts
-# end
+def vowel_counts(str)
+  counts = Hash.new(0)
+  vowels = "aeiou"
+  str.each_char { |char| counts[char.downcase] += 1 if vowels.include?(char.downcase) }
+  counts
+end
 
 def caesar_cipher(msg, num)
   alphabet = "abcdefghijklmnopqrstuvwxyz"
   encrypted = ""
-  
-  return msg if !alphabet.include?(char)
 
   msg.each_char do |char|
+    if !alphabet.include?(char)
+      encrypted += char
+      next
+    end
     shifted_idx = alphabet.index(char) + num
     shifted_char = alphabet[shifted_idx % 26]
     encrypted += shifted_char
@@ -34,5 +36,3 @@ def caesar_cipher(msg, num)
 
   encrypted
 end
-
-p caesar_cipher("123 _-!?@%", 3)
