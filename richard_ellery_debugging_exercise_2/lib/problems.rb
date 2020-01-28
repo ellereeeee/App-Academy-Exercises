312228177d7eb7ec007814a0887d2ab060ea9280
ellereeeee <raellery@gmail.com>
2020-01-27 22:26:57 -0500

    Refactor dupe_indices method

--- richard_ellery_debugging_exercise_2/lib/problems.rb
+++ richard_ellery_debugging_exercise_2/lib/problems.rb
@@ -5,13 +5,12 @@
 require 'byebug'
 
 def largest_prime_factor(num)
-  range = [*2..num]
-  composites = range.select do |val|
+  composites = (2..num).select do |val|
     num % val == 0
   end
   
   prime_composites = composites.select do |val|
-    [*2...val].all? { |val2| val % val2 != 0 }
+  (2...val).all? { |val2| val % val2 != 0 }
   end
 
   largest_prime_composite = prime_composites.max
@@ -30,29 +29,17 @@ def unique_chars?(str)
 end
 
 def dupe_indices(arr)
-  dupes_and_indices = {}
-  char_count = Hash.new(0)
+  indices = Hash.new{ |h, k| h[k] = [] }
   
-  # make hash with strings and times appeared in array arg
-  arr.each do |str|
-    char_count[str] += 1
+  arr.each.with_index do |key, idx|
+    indices[key] << idx
   end
 
-  # put strings that appeared more than once in dupes_and_indices
-  char_count.each do |key, val|
-    dupes_and_indices[key] = [] if val > 1
+  dupes = indices.select do |key, val|
+    val.length > 1
   end
 
-  # assign indices from array arg into corresponding vals in dupes_and_indices
-  dupes_and_indices.each_key do |key|
-    arr.each.with_index do |str, idx|
-      if str == key
-        dupes_and_indices[key] << idx
-      end
-    end
-  end
-  
-  dupes_and_indices
+  dupes
 end
 
 def ana_array(arr1, arr2)
