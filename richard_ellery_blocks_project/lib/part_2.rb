809ec59b272df9ee7f1bd8c3db09f5480c1b97b8
ellereeeee <raellery@gmail.com>
2020-02-06 21:10:52 -0500

    Refactor no valid url method

--- richard_ellery_blocks_project/lib/part_2.rb
+++ richard_ellery_blocks_project/lib/part_2.rb
@@ -2,17 +2,11 @@ def all_words_capitalized?(arr)
   arr.all? { |word| word == word.capitalize }
 end
 
-# helper method for no_valid_url?
-# returns true if url is valid
-
-def valid_url?(url)
-  valid_urls = [".com", ".net", ".io", ".org"]
-  url_ending = url[url.index(".")..-1]
-  valid_urls.include?(url_ending)
-end
-
 def no_valid_url?(urls)
-  urls.none? { |url| valid_url?(url) }
+  valid_urls = [".com", ".net", ".io", ".org"]
+  urls.none? do |url|
+    valid_urls.any? { |ending| url.end_with?(ending) }
+  end
 end
 
 # helper method for any_passing_students?
