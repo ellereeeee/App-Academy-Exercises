e3d44e748ef2ae58d162cfe2da865fe69372b8b1
ellereeeee <raellery@gmail.com>
2020-03-17 16:57:45 -0400

    Add bootcamp project files and part 1 solution

---
+++ Software Engineering Foundations/richard_ellery_bootcamp_project/lib/bootcamp.rb
@@ -0,0 +1,43 @@
+class Bootcamp
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new { |hash, k| hash[k] = [] }
+  end
+
+  def name
+    @name
+  end
+
+  def slogan
+    @slogan
+  end
+
+  def teachers
+    @teachers
+  end
+
+  def students
+    @students
+  end
+
+  def hire(teacher)
+    @teachers << teacher
+  end
+
+  def enroll(student)
+    if @students.length < @student_capacity
+      @students << student
+      return true
+    else
+      false
+    end
+  end
+
+  def enrolled?(student)
+    @students.include?(student)
+  end
+end
