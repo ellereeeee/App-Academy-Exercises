d2abc965dbe0d90123a6f33f454d22db27e24d9a
ellereeeee <raellery@gmail.com>
2020-04-10 14:43:30 -0400

    Add hotel class solution

--- Software Engineering Foundations/richard_ellery_hotel_project/lib/hotel.rb
+++ Software Engineering Foundations/richard_ellery_hotel_project/lib/hotel.rb
@@ -1,5 +1,43 @@
 require_relative "room"
 
 class Hotel
-  
+  def initialize(name, rooms)
+    @name = name
+    @rooms = rooms.each { |k,v| rooms[k] = Room.new(v) }
+  end
+
+  def name
+    upcased_name = @name.split(" ").map(&:capitalize).join(" ")
+    upcased_name
+  end
+
+  def rooms
+    @rooms
+  end
+
+  def room_exists?(room)
+    @rooms.include?(room)
+  end
+
+  def check_in(person, room_name)
+    if !room_exists?(room_name)
+      puts "sorry, room does not exist"
+    else
+      if @rooms[room_name].add_occupant(person)
+        puts "check in successful"
+      else
+        puts "sorry, room is full"
+      end
+    end
+  end
+
+  def has_vacancy?
+    @rooms.values.any? { |room| room.available_space > 0 }
+  end
+
+  def list_rooms
+    @rooms.each do |name, room|
+      puts "#{name} : #{room.available_space}"
+    end
+  end
 end
