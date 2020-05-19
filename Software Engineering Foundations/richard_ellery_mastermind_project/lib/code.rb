42bb55b736f45dbc7f48012ba2cfa9251b338779
ellereeeee <raellery@gmail.com>
2020-05-18 23:49:35 -0400

    Add mastermind project code part 1

---
+++ Software Engineering Foundations/richard_ellery_mastermind_project/lib/code.rb
@@ -0,0 +1,42 @@
+class Code
+  POSSIBLE_PEGS = {
+    "R" => :red,
+    "G" => :green,
+    "B" => :blue,
+    "Y" => :yellow
+  }
+  
+  def self.valid_pegs?(chars)
+    chars.map(&:upcase).all? { |color| POSSIBLE_PEGS.keys.include?(color) }
+  end
+
+  def initialize(chars)
+    if !Code.valid_pegs?(chars)
+      raise "contains invalid peg(s)"
+    else
+      @pegs = chars.map(&:upcase)
+    end
+  end
+
+  def pegs
+    @pegs
+  end
+
+  def self.random(length)
+    random_pegs = []
+    length.times { random_pegs << POSSIBLE_PEGS.keys.sample }
+    Code.new(random_pegs)
+  end
+
+  def self.from_string(string)
+    Code.new(string.split(""))
+  end
+
+  def [](index)
+    @pegs[index]
+  end
+
+  def length
+    @pegs.length
+  end
+end
