4127d7eb43557842a68aacbed683778802bff75d
ellereeeee <raellery@gmail.com>
2020-07-29 07:24:23 -0400

    Add methods that place random ships

--- Software Engineering Foundations/richard_ellery_battleship_project/lib/board.rb
+++ Software Engineering Foundations/richard_ellery_battleship_project/lib/board.rb
@@ -30,4 +30,24 @@ class Board
       false
     end
   end
+  
+  def random_position
+    max_length = @grid[0].length
+    random_row = rand(1...max_length)
+    random_col = rand(1...max_length)
+    position = [random_row, random_col]
+  end
+
+  def place_random_ships
+    amount_to_place = (@grid.flatten.length * 0.25).round
+    amount_to_place.times do
+      position = self.random_position
+      if self.[](position) == :S
+        until self.[](position) != :S
+          position = self.random_position
+        end
+      end
+      self.[]=(position, :S)
+    end
+  end
 end
