8ce9b4ae6682ee58eba40b117ccec320e43ff258
ellereeeee <raellery@gmail.com>
2020-07-31 01:23:20 -0400

    Write initalize method for battleship class

--- Software Engineering Foundations/richard_ellery_battleship_project/lib/battleship.rb
+++ Software Engineering Foundations/richard_ellery_battleship_project/lib/battleship.rb
@@ -2,5 +2,9 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
-
+  def initialize(n)
+    @player = Player.new
+    @board = Board.new(n)
+    @remaining_misses = @board.size / 2
+  end
 end
