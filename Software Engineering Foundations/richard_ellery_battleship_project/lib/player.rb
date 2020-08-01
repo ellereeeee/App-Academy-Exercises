75f14714f712a2d13172f98488439946e9c97ec5
ellereeeee <raellery@gmail.com>
2020-07-31 20:57:05 -0400

    Refactor get move method

--- Software Engineering Foundations/richard_ellery_battleship_project/lib/player.rb
+++ Software Engineering Foundations/richard_ellery_battleship_project/lib/player.rb
@@ -1,7 +1,6 @@
 class Player
   def get_move
     puts 'enter a position with coordinates separated with a space like `4 7`'
-    input = gets.chomp.split(" ")
-    input.map { |ele| ele.to_i }
+    gets.chomp.split(" ").map(&:to_i)
   end
 end
